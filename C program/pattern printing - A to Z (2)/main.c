af80e03404e71b6304ff3a689a7340e390231c58
sian-julian <sianjulian.prof@gmail.com>
2024-07-09 10:03:05 +0530

    9/07/2024

--- C program/pattern printing - A to Z (2)/main.c
+++ C program/pattern printing - A to Z (2)/main.c
@@ -4,13 +4,16 @@
 int main()
 {
     int i,j;
-    char n='E';
+    
     for(i=0;i<=5;i++) 
     {
+        char n='A';
         for(j=0;j<=i;j++) 
         {
-            printf("%c ",'A'+j);
+            printf("%c ",n);
+            n++;
         }
+        
         printf("\n");
     }
 
