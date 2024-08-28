bb9a0da643ea908fb8bfa54793dbafcf0befe5c5
sian-julian <sianjulian.prof@gmail.com>
2024-08-28 09:15:08 +0530

    rep9

--- Python/Complex Operations/main.py
+++
@@ -1,8 +0,0 @@
-a=3 + 4j 
-b= 1 + 2j
-
-sum=a+b
-product=a*b
-
-print(f"sum of {a} and {b}:{sum}")
-print(f"product of {a} and {b}:{product}")
--- Python/List Operations/main.py
+++
@@ -1,8 +0,0 @@
-list1=[1,2,3,4,5]
-list2=[7,8,9]
-
-list1.append(6)
-print("After Appending 6:",list1)
-
-list1.extend(list2)
-print("After Extension:",list1)
